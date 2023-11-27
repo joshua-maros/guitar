90251977b78d46b104bc22dc60755f4153a7a79e
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2023-11-26 16:33:20 -0800

    Working better.

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,10 @@ fn main() {
 
     let mut canvas = Canvas::new(512, 512);
     for step in 0..5_000_000 {
-        let dt = 0.000_02;
-        let t = step as f64 / 100.0 * dt;
+        let dt = 0.000_000_2;
+        let t = step as f64 * dt;
 
-        if step % 1000 == 0 {
+        if step % 100 == 0 {
             canvas.clear(0.0);
             let mut points = Vec::new();
             // Converts the guitar string's points into a series of coordinates to be displayed on the screen.
@@ -54,7 +54,7 @@ fn main() {
                 let y = p.displacement;
                 // let y = displacement_derivative[i];
                 // let y = displacement_curvature[i];
-                points.push(Vec2::new(x as f32, y.map_range(-1.0..1.0, 1.0..0.0) as f32));
+                points.push(Vec2::new(x as f32, y.map_range(-0.01..0.01, 1.0..0.0) as f32));
             }
             canvas.draw_path(&points, 1.0);
             canvas.show();
@@ -63,43 +63,25 @@ fn main() {
             }
         }
 
-        // Computes the first derivative of displacement according to position
-        let mut displacement_derivative = [0.0; GUITAR_STRING_POINTS];
-        for i in 1..GUITAR_STRING_POINTS - 1 {
-            displacement_derivative[i] = 0.5
-                * (string.points[i + 1].displacement - string.points[i - 1].displacement)
-                / (GUITAR_STRING_LENGTH / (GUITAR_STRING_POINTS - 1) as f64);
-        }
-        let mut filtered = [0.0; GUITAR_STRING_POINTS];
-        for i in 1..GUITAR_STRING_POINTS - 1 {
-            filtered[i] = 0.25
-                * (displacement_derivative[i + 1]
-                    + 2.0 * displacement_derivative[i]
-                    + displacement_derivative[i - 1]);
-        }
-        displacement_derivative = filtered;
-
         // Computes the curvature of displacement according to position
         let mut displacement_curvature = [0.0; GUITAR_STRING_POINTS];
         for i in 1..GUITAR_STRING_POINTS - 1 {
-            displacement_curvature[i] = 0.5
-                * (displacement_derivative[i + 1] - displacement_derivative[i - 1])
-                / (GUITAR_STRING_LENGTH / (GUITAR_STRING_POINTS - 1) as f64);
+            displacement_curvature[i] = (string.points[i + 1].displacement
+                + string.points[i - 1].displacement
+                - 2.0 * string.points[i].displacement)
+                / (GUITAR_STRING_LENGTH / (GUITAR_STRING_POINTS - 1) as f64).powi(2);
         }
-        displacement_curvature[0] = displacement_curvature[2];
-        displacement_curvature[1] = displacement_curvature[2];
+        displacement_curvature[0] = displacement_curvature[1];
         displacement_curvature[GUITAR_STRING_POINTS - 1] =
-            displacement_curvature[GUITAR_STRING_POINTS - 3];
-        displacement_curvature[GUITAR_STRING_POINTS - 2] =
-            displacement_curvature[GUITAR_STRING_POINTS - 3];
+            displacement_curvature[GUITAR_STRING_POINTS - 2];
 
         // Applies various forces to the string.
         for (i, p) in string.points.iter_mut().enumerate() {
             let x = i as f64 / (GUITAR_STRING_POINTS - 1) as f64;
-            let finger_force = pluck_force(x, t) / GUITAR_STRING_DENSITY;
+            let finger_force = pluck_force(x, t);
             // let finger_force = 0.0;
             let tension_force = GUITAR_STRING_TENSION * displacement_curvature[i];
-            p.velocity += (finger_force + tension_force) * dt;
+            p.velocity += (finger_force + tension_force) / GUITAR_STRING_DENSITY * dt;
         }
 
         // Applies the velocity of the string to itself.
